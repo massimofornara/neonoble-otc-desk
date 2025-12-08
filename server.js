5e2a03f0414b6fe2d1a0106002fc6d5758264708
massimofornara <mfornara93@gmail.com>
2025-12-08 20:37:47 +0100

    Update OTC controller path in server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .catch(err => console.error('MongoDB errore:', err));
 
 // Rotte API
-app.use('/api/otc', require('./controllers/otcController'));
+app.use('/api/otc', require('./backend/controllers/otcController'));
 
 // Servi frontend
 app.use(express.static(path.join(__dirname, '../frontend')));
